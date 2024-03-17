000760860f34d0997fde9795279c3b51c84794d6
Mihai Lambru <mihailambru99@gmail.com>
2024-03-17 16:42:13 +0200

    final commit

--- populate_db.sh
+++ populate_db.sh
@@ -1,14 +1,9 @@
 kubectl port-forward service/postgresql-service 5433:5432 &
 
-kubectl port-forward service/postgresql-service 5433:5432 &
-
-# Capture the PID of the port-forward process
 PF_PID=$!
 
-# Set the database password
 export PGPASSWORD="mypassword"
 
-# Wait for the port-forward to be established by checking the port availability
 while ! nc -z localhost 5433; do   
   echo "Waiting for port-forwarding to be established..."
   sleep 1
